d879345987280a744ee8521b1999eb419a36edfc
Zakhozhij <maxzahojii@mail.ru>
2023-05-12 16:41:59 +0300

    comm

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,23 +1,17 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", () => {
-	getOffset();
+	//getOffset();
 });
 
 //Footer margin from top
 window.addEventListener(
 	`resize`,
 	() => {
-		getOffset();
+		//getOffset();
 	},
 	false
 );
 
-function getOffset() {
-	const form_position = document.querySelector(".company-partners").getBoundingClientRect().bottom;
-	const promo_position = document.querySelector(".promo").getBoundingClientRect();
-	document.querySelector(".info_block").style.marginTop = form_position - promo_position.bottom + 35 + "px";
-	document.querySelector("footer").style.marginTop = form_position - promo_position.bottom + 35 + 200 + "px";
-}
 
 
 
